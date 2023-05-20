18b0f0a859034c7aa7d1643020d8acab7d7575cc
averagepythonfan <djonvorobei@bk.ru>
2023-05-20 14:56:18 +0300

    conf

--- fastapi_app/config.py
+++ fastapi_app/config.py
@@ -1,9 +1,9 @@
 import os
 
 
-MONGONET: str = '172.19.0.2' #os.getenv('MONGONET')
+MONGONET: str = os.getenv('MONGONET')
 TOKEN: str = os.getenv('TOKEN')
 REDIS: str = os.getenv('REDIS')
 MLFLOW_CLIENT: str = os.getenv('MLFLOW_CLIENT')
 MLFLOW_SERVER: str = os.getenv('MLFLOW_SERVER')
-DATABASE: str = 'test_db' #os.getenv('DATABASE')
+DATABASE: str = os.getenv('DATABASE')
